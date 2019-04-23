65affee96a22830300403616832197c36959b52e
Daylon <dcrider1@asu.edu>
2019-04-23 15:11:22 -0700

    Table is populating correctly

--- db-handler.js
+++ db-handler.js
@@ -14,45 +14,33 @@ MySql.Execute(
     // Get the table to list the services
     var table = document.getElementById("services-table");
 
-    var dataAsJSON = JSON.stringify(data);
-    console.log(dataAsJSON);
-    // Add an empty <tr> element at the last position of the table
-    var row = table.insertRow(-1);
+    var dataAsJSON = data["Result"];
+    console.log(dataAsJSON[0]);
 
-    // Add empty cells for all the info we need to show
-    var cellName = row.insertCell(0);
-    var cellPrice = row.insertCell(1);
-    var cellDesc = row.insertCell(2);
-    var cellHrs = row.insertCell(3);
+    // Loop through the returned data and add it to the table
+	var i;
+    for(i = 0; i < dataAsJSON.length; i++){
+		//console.log(dataAsJSON[0]);
+		// Add an empty <tr> element at the last position of the table
+		var row = table.insertRow(-1);
 
-    // Populate the cells
-    cellName.innerHTML = "";
-    cellPrice.innerHTML = "";
-    cellDesc.innerHTML = "";
-    cellHrs.innerHTML = "";
+		// Add empty cells for all the info we need to show
+		var cellName = row.insertCell(0);
+		var cellPrice = row.insertCell(1);
+		var cellDesc = row.insertCell(2);
+		var cellHrs = row.insertCell(3);
 
-    // Set the id of the field
-    cellName.id = "namedisp";
-    cellPrice.id = "pricedisp";
-    cellDesc.id = "descdisp";
-    cellHrs.id = "hrsdisp";
-    }
+		// Populate the cells
+		cellName.innerHTML = dataAsJSON[i]["Name"];
+		cellPrice.innerHTML = dataAsJSON[i]["Price"];
+		cellDesc.innerHTML = dataAsJSON[i]["Desc"];
+		cellHrs.innerHTML = dataAsJSON[i]["EstimatedTime"];
+
+		// Set the id of the field
+		cellName.id = "namedisp";
+		cellPrice.id = "pricedisp";
+		cellDesc.id = "descdisp";
+		cellHrs.id = "hrsdisp";
+	}
+  }
 );
-/*
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-    host: "sql.wpc-is.online", 
-    user: "teama03", 
-    password: "teama039832", 
-    database: "db_teama03"
-});
-console.log("Attempting connection to DB Server...");
-con.connect(function(err) {
-  if (err) throw err;
-  con.query("SELECT * FROM Services", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-    finalData = result;
-  });
-});*/
